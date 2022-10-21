bcdcea5a76285c023f294ca123f0021d7291c21d
anniexp <59566543+anniexp@users.noreply.github.com>
2022-10-21 11:03:59 +0300

    adding initialized messenger project folder

---
+++ messenger-attachments/src/test/java/com/ssjavaacademy/www/messengerattachments/MessengerAttachmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssjavaacademy.www.messengerattachments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerAttachmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
